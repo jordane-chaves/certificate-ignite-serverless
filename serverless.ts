939bb3c6b04b34cbd0ca680d778dc2b193a6d313
jordane-chaves <jordane_chaves@yahoo.com.br>
2022-09-29 21:11:06 -0300

    Configure deploy

--- serverless.ts
+++ serverless.ts
@@ -19,45 +19,48 @@ const serverlessConfiguration: AWS = {
     },
     iamRoleStatements: [
       {
-        Effect: "Allow",         // O effect que irá fazer
-        Action: ["dynamodb:*"],  // Onde será realizado o effect, neste caso será liberada todas as tabelas do dynamodb
-        Resource: ["*"],         // Libera para todos os Resources configurados
+        Effect: 'Allow',         // O effect que irá fazer
+        Action: ['dynamodb:*'],  // Onde será realizado o effect, neste caso será liberada todas as tabelas do dynamodb
+        Resource: ['*'],         // Libera para todos os Resources configurados
       },
       {
-        Effect: "Allow",
-        Action: ["s3:*"],
-        Resource: ["*"],
+        Effect: 'Allow',
+        Action: ['s3:*'],
+        Resource: ['*'],
       }
     ],
   },
   // import the function via paths
   functions: {
     generateCertificate: {                                  // Nome da função
-      handler: "src/functions/generateCertificate.handler", // Caminho da função criada
+      handler: 'src/functions/generateCertificate.handler', // Caminho da função criada
       events: [                                             // Eventos disponíveis para nossa função
         {
           http: {                                           // Existem vários, mas vamos utilizar o HTTP
-            path: "generateCertificate",                    // Caminho para chamar a função
-            method: "post",                                 // Método da requisição
+            path: 'generateCertificate',                    // Caminho para chamar a função
+            method: 'post',                                 // Método da requisição
             cors: true,                                     // Habilita o cors
           },
         }
       ],
     },
     verifyCertificate: {
-      handler: "src/functions/verifyCertificate.handler",
+      handler: 'src/functions/verifyCertificate.handler',
       events: [
         {
           http: {
-            path: "verifyCertificate/{id}",
-            method: "get",
+            path: 'verifyCertificate/{id}',
+            method: 'get',
             cors: true,
           }
         }
       ],
     }
   },
-  package: { individually: true },
+  package: {
+    individually: false,              // Alteramos para `false` para gerar apenas um ZIP com todas as funções
+    include: ['./src/templates/**'],  // Para incluir todos os arquivos do diretório templates no deploy
+  },
   custom: {
     esbuild: {
       bundle: true,
@@ -71,7 +74,7 @@ const serverlessConfiguration: AWS = {
       external: ['chrome-aws-lambda'],
     },
     dynamodb: {
-      stages: ['dev', 'local'], // Infoma quando deve utilizar o DynamoDB Local
+      stages: ['dev', 'local'], // Informa quando deve utilizar o DynamoDB Local
       start: {
         port: 8000,             // Porta padrão do DynamoDB
         inMemory: true,         // Essa opção informa para executar em memória
@@ -82,23 +85,23 @@ const serverlessConfiguration: AWS = {
   resources: {
     Resources: {
       dbCertificateUsers: {
-        Type: "AWS::DynamoDB::Table",
+        Type: 'AWS::DynamoDB::Table',
         Properties: {
-          TableName: "users_certificate",
+          TableName: 'users_certificate',
           ProvisionedThroughput: {
             ReadCapacityUnits: 5,
             WriteCapacityUnits: 5,
           },
           AttributeDefinitions: [
             {
-              AttributeName: "id",
-              AttributeType: "S",
+              AttributeName: 'id',
+              AttributeType: 'S',
             }
           ],
           KeySchema: [
             {
-              AttributeName: "id",
-              KeyType: "HASH",
+              AttributeName: 'id',
+              KeyType: 'HASH',
             }
           ],
         }
